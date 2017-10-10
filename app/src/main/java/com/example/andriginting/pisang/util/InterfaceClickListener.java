e74657c5f3be34c07bae3e95c6e84c574772708e
Andri Ginting <andris.ginting@gmail.com>
2017-10-10 10:07:46 +0700

    add interface click

---
+++ app/src/main/java/com/example/andriginting/pisang/util/InterfaceClickListener.java
@@ -0,0 +1,11 @@
+package com.example.andriginting.pisang.util;
+
+import android.view.View;
+
+/**
+ * Created by Andri Ginting on 10/10/2017.
+ */
+
+public interface InterfaceClickListener {
+    public void ROnclickListener(View v, int position);
+}
